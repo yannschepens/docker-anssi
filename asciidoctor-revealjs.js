8d21e2f098fcf7833b4c20fcdef6d4dd493cfbc3
Yann Schepens <y.schepens@groupeonepoint.com>
2022-06-03 09:58:44 +0200

    feat: move files on dist directory

--- asciidoctor-revealjs.js
+++ asciidoctor-revealjs.js
@@ -4,5 +4,5 @@ var asciidoctorRevealjs = require('@asciidoctor/reveal.js');
 asciidoctorRevealjs.register();
 
 // Convert the document 'presentation.adoc' using the reveal.js converter
-var options = {safe: 'safe', backend: 'revealjs'};
+var options = {safe: 'safe', backend: 'revealjs', to_dir: 'dist', mkdirs: true};
 asciidoctor.convertFile('index.adoc', options);
