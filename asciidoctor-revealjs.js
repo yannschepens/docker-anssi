480d70bb6e0968129fc9207f89e8b714998439ec
Yann Schepens <y.schepens@groupeonepoint.com>
2024-02-15 14:07:00 +0100

    chore: update conf with new dir structure

--- asciidoctor-revealjs.js
+++ asciidoctor-revealjs.js
@@ -1,8 +1,13 @@
-// Load asciidoctor.js and asciidoctor-reveal.js
-var asciidoctor = require('@asciidoctor/core')();
-var asciidoctorRevealjs = require('@asciidoctor/reveal.js');
+const fs = require('fs');
+const asciidoctor = require('@asciidoctor/core')();
+const asciidoctorRevealjs = require('@asciidoctor/reveal.js');
 asciidoctorRevealjs.register();
 
-// Convert the document 'presentation.adoc' using the reveal.js converter
-var options = {safe: 'safe', backend: 'revealjs', to_dir: '.', mkdirs: true, standalone: true};
+const distDirPath = './dist';
+
+const options = {safe: 'safe', backend: 'revealjs', to_dir: distDirPath, mkdirs: true, standalone: true};
 asciidoctor.convertFile('index.adoc', options);
+
+fs.cp('./styles', distDirPath + '/styles', {recursive: true}, (err) => console.error);
+fs.cp('./assets', distDirPath + '/assets', {recursive: true}, (err) => console.error);
+fs.cp('./node_modules/reveal.js', distDirPath + '/node_modules/reveal.js', {recursive: true}, (err) => console.error);
